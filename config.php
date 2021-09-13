39ce048f5cfa342b2f20fa78b1ecbba31eb3d7cb
Jacob Dennis P <jacoden143@gmail.com>
2021-09-13 09:10:45 +0530

    updated config.php

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php 
 
+    define('DB_SERVER','localhost');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    define('DB_DATABASE','vvt_proj_2');
+    
+    $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+    if($connection===false){
+        die("Error : Could not connect <br> Error Stack Trace :<br>".mysqli_connect_error());
+    }
+
 ?>
