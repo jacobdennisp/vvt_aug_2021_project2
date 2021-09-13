98b44da81f79300752b17ad7916e37bb40c6461b
Jacob Dennis P <jacoden143@gmail.com>
2021-09-13 09:43:40 +0530

    spacing

--- server.php
+++ server.php
@@ -4,9 +4,7 @@
 
     if($_POST['operation_type']=='register'){
 
-
         $sql = "INSERT INTO users(name,phone_no,email,password) VALUES (?,?,?,?)";
-
         if($statement=mysqli_prepare($connection,$sql)){
             mysqli_stmt_bind_param($statement,"ssss",$username,$phoneno,$emailid,$password);
 
@@ -15,13 +13,11 @@
             $emailid=$_POST['email'];
             $password= password_hash($_POST['password'],PASSWORD_DEFAULT);
         }
-
         if(mysqli_stmt_execute($statement)){
             header("location:index.php");
         }else{
             echo "Oops SOmething went wrong";
         }
-
         mysqli_stmt_close($statement);
 
     }
